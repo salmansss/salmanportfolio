63392a6ef32c78f0fec74b9e2e234a60f37fedf4
salmansss <attitude007780@gmail.com>
2020-08-10 09:56:02 +0530

    salmanportfolio3

--- resume/settings.py
+++ resume/settings.py
@@ -76,9 +76,20 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # for the sqlite3 databases connector
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    # here we connect database with the postresql
+
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resume',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
